f1192c884411a0ad78b0816422f123f4eeec9be3
sunny-yadav <sunny9405683@gmail.com>
2024-12-08 13:03:39 +0530

    upated the path of multer file

--- Backend/src/Routers/user.Router.js
+++ Backend/src/Routers/user.Router.js
@@ -1,5 +1,5 @@
 import Router from 'express'
-import {upload} from '../Middlewares/multer.js'
+import {upload} from '../Middlewares/multer.middleware.js'
 import { login_User, register_User } from '../controllers/user.controller.js';
 
 const router = Router();
