5fee3644e3f75de7cd39bf4ba9c504b91ee7add6
sunny-yadav <sunny9405683@gmail.com>
2024-12-08 15:34:37 +0530

    updated the file path of provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { UserContextProvider } from './ContextApi/userContext.jsx'
+import { UserContextProvider } from './ContextApi/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <UserContextProvider>
         <App />
     </UserContextProvider>
+
+
 )
