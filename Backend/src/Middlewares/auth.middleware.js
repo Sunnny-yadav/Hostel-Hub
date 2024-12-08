32809c7efc9da5fda4d52cbda4df33ed60e87f95
sunny-yadav <sunny9405683@gmail.com>
2024-12-08 13:18:28 +0530

    created a auth middleware

---
+++ Backend/src/Middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import { User } from "../Models/user.model";
+import { AsyncHandeller } from "../utils/AsyncHandeller";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = AsyncHandeller(async (req, res, next) => {
+  const Token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!Token) {
+    res.status(400).json({
+      Error: "Token not avilable :: unauthorized user",
+    });
+  }
+
+  const decodedToken = jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET_KEY);
+
+  const user = await User.findOne(decodedToken?._id).select("-password");
+
+  if (!user) {
+    res.status(400).json({
+      Error: "Invalid user Token",
+    });
+  }
+
+  req.user = user;
+  next();
+});
