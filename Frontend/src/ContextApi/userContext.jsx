7aea23bcc8ee421fd3aaad6d1d7865e59512702d
sunny-yadav <sunny9405683@gmail.com>
2024-12-08 16:49:34 +0530

    created state for data display

--- Frontend/src/ContextApi/userContext.jsx
+++ Frontend/src/ContextApi/userContext.jsx
@@ -1,20 +1,60 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const User_context = createContext();
 
-export const UserContextProvider = ({children})=>{
+export const UserContextProvider = ({ children }) => {
+    const [AccessToken, setAccessToken] = useState(JSON.parse(localStorage.getItem("AccessToken")) || null);
+    const [userData, setuserData] = useState("");
+    
+    
+    //fetch loged in user data
+    const fetchUserData = async () => {
+        try {
+            let response = await fetch("http://localhost:8000/api/v1/users/getUserData", {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${AccessToken}`
+                }
+            });
+            if (response.ok) {
+                let fetchedData = await response.json();
+                console.log(fetchedData.data)
+                setuserData(fetchedData.data);
+            } else {
+                throw new Error("User Data not fetched successfully")
+            }
 
-let value  = 1;
+        } catch (error) {
+            console.log(`Frontend::ContextApi::UserContext::${error.msg}`);
+        }
+
+    };
+
+    // store token in local storage
+    const SetTokenInLocalStorage = (Token) => {
+        localStorage.setItem("AccessToken", JSON.stringify(Token))
+        setAccessToken(Token)
+    };
+
+    const Logout= () =>{
+        localStorage.removeItem("AccessToken")
+        setAccessToken(null)
+    }
+
+    useEffect(() => {
+        AccessToken && fetchUserData();
+    }, [AccessToken]);
 
 
-    return(
-        <User_context.Provider value={{value}}>
-        {children}
+
+    return (
+        <User_context.Provider value={{ SetTokenInLocalStorage , userData, Logout}}>
+            {children}
         </User_context.Provider>
     )
 };
 
 
-export const UserContext= ()=>{
+export const UserContext = () => {
     return useContext(User_context);
 }
