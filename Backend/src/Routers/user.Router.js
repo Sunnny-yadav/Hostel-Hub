693e9be5002aca8da69c4397d2403906422d50a1
sunny-yadav <sunny9405683@gmail.com>
2024-12-18 23:52:40 +0530

    added the validate schema middleware in routes

--- Backend/src/Routers/user.Router.js
+++ Backend/src/Routers/user.Router.js
@@ -2,10 +2,12 @@ import Router from 'express'
 import {upload} from '../Middlewares/multer.middleware.js'
 import { getLogedInUserData, login_User, register_User } from '../controllers/user.controller.js';
 import { verifyJWT } from '../Middlewares/auth.middleware.js';
+import { validateShema } from '../Middlewares/validator.middleware.js';
+import { RegistrationSchema } from '../Validators/auth.validator.js';
 
 const router = Router();
 
-router.route("/register").post(upload.single("avatar"), register_User)
+router.route("/register").post(upload.single("avatar"),validateShema(RegistrationSchema), register_User)
 router.route("/login").post(login_User);
 router.route("/getUserData").get(verifyJWT, getLogedInUserData);
 
