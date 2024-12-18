a9dcd895e1907848e652b46f3868ab4047db16cd
sunny-yadav <sunny9405683@gmail.com>
2024-12-19 02:05:31 +0530

    validator middleware finalized

--- Backend/src/Middlewares/validator.middleware.js
+++ Backend/src/Middlewares/validator.middleware.js
@@ -1,12 +1,12 @@
 export const validateShema = (schema) => async (req, res, next) => {
-    try {
-      console.log("Request Body Before Validation:", req.body);
-      const parsebody = await schema.parseAsync(req.body);
-      // req.body = parsebody;  
-      next();
-    } catch (error) {
-      console.log("Validation Error:", error);
-      res.status(400).json({ msg: 'Validation failed from Zod', error });
-    }
+  try {
+    const parsebody = await schema.parseAsync(req.body);
+    // req.body = parsebody;
+    next();
+  } catch (err) {
+    const status = 400;
+    const message = err.issues[0].message;
+    const error = { status, message };
+    next(error);
   }
-  
+};
