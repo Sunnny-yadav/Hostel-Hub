48465eb0e67258c3646d5065827f097316e59820
sunny-yadav <sunny9405683@gmail.com>
2024-12-20 00:19:38 +0530

    added Route for editing complaint state

--- Backend/src/Routers/complain.Router.js
+++ Backend/src/Routers/complain.Router.js
@@ -1,5 +1,5 @@
 import Router from 'express'
-import { edit_Complaint, insert_comment, register_Complaint } from '../controllers/complaint.controller.js';
+import { edit_Complaint, edit_Complaint_State, insert_comment, register_Complaint } from '../controllers/complaint.controller.js';
 import { verifyJWT } from '../Middlewares/auth.middleware.js';
 import { upload } from '../Middlewares/multer.middleware.js';
 
@@ -8,5 +8,6 @@ const router = Router();
 router.route('/register-complaint').post(verifyJWT,upload.single("image"),register_Complaint);
 router.route('/:complaintId/insert-comment').post(verifyJWT,insert_comment);
 router.route('/:complaintId/edit-Complaint').patch(verifyJWT,upload.single("image"),edit_Complaint);
+router.route('/:complaintId/edit-complaint-state').patch(verifyJWT,edit_Complaint_State)
 
 export default router
