2d778e8d975ef83573acc3573f4ab6e0de4d171f
sunny-yadav <sunny9405683@gmail.com>
2024-12-21 14:52:31 +0530

    route for get complaints by id and state declared

--- Backend/src/Routers/complain.Router.js
+++ Backend/src/Routers/complain.Router.js
@@ -1,5 +1,5 @@
 import Router from 'express'
-import { edit_Complaint, edit_Complaint_State, insert_comment, register_Complaint } from '../controllers/complaint.controller.js';
+import { edit_Complaint, edit_Complaint_State, get_Complaints_By_Id_State, insert_comment, register_Complaint } from '../controllers/complaint.controller.js';
 import { verifyJWT } from '../Middlewares/auth.middleware.js';
 import { upload } from '../Middlewares/multer.middleware.js';
 
@@ -8,6 +8,7 @@ const router = Router();
 router.route('/register-complaint').post(verifyJWT,upload.single("image"),register_Complaint);
 router.route('/:complaintId/insert-comment').post(verifyJWT,insert_comment);
 router.route('/:complaintId/edit-Complaint').patch(verifyJWT,upload.single("image"),edit_Complaint);
-router.route('/:complaintId/edit-complaint-state').patch(verifyJWT,edit_Complaint_State)
+router.route('/:complaintId/edit-complaint-state').patch(verifyJWT,edit_Complaint_State);
+router.route('/:state/get-Complaints').get(verifyJWT,get_Complaints_By_Id_State);
 
-export default router
+export default router;
