b147ffa6b852cc61fe590381d2cc210fb3d7e7ef
sunny-yadav <sunny9405683@gmail.com>
2024-12-27 00:45:47 +0530

    removed the unwanted slices

--- Frontend/src/Redux/Store.js
+++ Frontend/src/Redux/Store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import TermReducers from './slices/TermsConditionSlice.js'
-import MealReducers from './slices/MealSlice.js'
+
 
 export const store = configureStore({
     reducer:{
         TermCondition : TermReducers,
-        Meal : MealReducers
     }
 })
