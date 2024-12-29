16602f181f04c7643c7cbb4655abf72e1edaac42
sunny-yadav <sunny9405683@gmail.com>
2024-12-30 02:02:18 +0530

    created function to fetch complaint and sort

--- Frontend/src/Context/complaintContext.jsx
+++ Frontend/src/Context/complaintContext.jsx
@@ -1,13 +1,16 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { useUserContext } from './userContext';
+import { toast } from 'react-toastify'
 
 const complaintContext = createContext();
 
 export const ComplaintContextProvider = ({ children }) => {
     const [FetchedComplaintsById, setFetchComplaintsById] = useState([])
+    const [FetchedComplaintsByIdAndType, setFetchComplaintsByIdAndType] = useState({});
     const { Token, AccessToken } = useUserContext();
+    const [complaintsToBeDisplayed, setcomplaintsToBeDisplayed] = useState(FetchedComplaintsByIdAndType);
 
-    const getUserComplaints = async () => {
+    const getUserComplaintsById = async () => {
         try {
             const response = await fetch("http://localhost:8000/api/v1/complaints/get-complaint-by-id", {
                 method: "GET",
@@ -28,20 +31,59 @@ export const ComplaintContextProvider = ({ children }) => {
         }
     };
 
-    
+    const getComplaintsByIdAndType = async (Type) => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/v1/complaints/${Type}/get-complaints-by-id-type`, {
+                method: "GET",
+                headers: {
+                    Authorization: Token
+                }
+            });
 
+            const responseData = await response.json();
+            console.log(responseData);
+            if (response.ok) {
+                setFetchComplaintsByIdAndType(responseData.data);
+                setcomplaintsToBeDisplayed(responseData.data);
+            } else {
+                toast.error(responseData.message)
+            };
 
-    useEffect(() => {
-        AccessToken && getUserComplaints();
-    }, [AccessToken])
-
+        } catch (error) {
+            console.error("complaintContext.jsx :: getcomplaintsByidAndType() ->", error.message)
+        }
+    }
 
     const addNewComplaintInComplaintArray = (newComplaint) => {
         setFetchComplaintsById((prev) => [newComplaint, ...prev]);
     }
 
+    const filterFetchedComplaints = (state)=>{
+        const complaints = FetchedComplaintsByIdAndType?.complaints.filter((complaint)=> complaint.state === state);
+        const count = complaints.length 
+        setcomplaintsToBeDisplayed({
+            complaints,
+            count
+        });
+    }
+
+    useEffect(() => {
+        AccessToken && getUserComplaintsById();
+    }, [AccessToken])
+
+
+
     return (
-        <complaintContext.Provider value={{ FetchedComplaintsById, addNewComplaintInComplaintArray }}>
+        <complaintContext.Provider
+            value={
+                {
+                    FetchedComplaintsById,
+                    addNewComplaintInComplaintArray,
+                    getComplaintsByIdAndType,
+                    complaintsToBeDisplayed,
+                    filterFetchedComplaints
+                }
+            }>
             {children}
         </complaintContext.Provider>
     )
