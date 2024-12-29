78dc8512931ba0b33f4d26464852f663eee8b603
sunny-yadav <sunny9405683@gmail.com>
2024-12-29 16:40:47 +0530

    configured the complaint context provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,16 +3,18 @@ import App from './App.jsx'
 import './index.css'
 import { Flip, ToastContainer } from 'react-toastify'
 import { UserContextProvider } from './Context/userContext.jsx'
-import { complaintContextProvider } from './Context/complaintContext.jsx'
-
+import { ComplaintContextProvider } from './Context/complaintContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <complaintContextProvider>
-        <UserContextProvider>
+
+
+    <UserContextProvider>
+        <ComplaintContextProvider>
             <App />
             <ToastContainer autoClose={3000} theme='colored' transition={Flip} />
-        </UserContextProvider>
-    </complaintContextProvider>
+        </ComplaintContextProvider>
+    </UserContextProvider>
+
 
 
 )
