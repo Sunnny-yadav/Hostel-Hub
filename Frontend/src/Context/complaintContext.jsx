854c214f3ac7a735f1286ba082eafe28eb520a7c
sunny-yadav <sunny9405683@gmail.com>
2024-12-31 01:47:31 +0530

    delte complaint funcionality

--- Frontend/src/Context/complaintContext.jsx
+++ Frontend/src/Context/complaintContext.jsx
@@ -5,12 +5,14 @@ import { toast } from 'react-toastify'
 const complaintContext = createContext();
 
 export const ComplaintContextProvider = ({ children }) => {
+    // const [complaintsMap, setComplaintsMap] = useState(new Map())
+    const [complaintsnotPresent, setcomplaintnotPresent] = useState(false)
     const [FetchedComplaintsById, setFetchComplaintsById] = useState([])
     const [FetchedComplaintsByIdAndType, setFetchComplaintsByIdAndType] = useState({});
     const { Token, AccessToken } = useUserContext();
     const [complaintsToBeDisplayed, setcomplaintsToBeDisplayed] = useState(FetchedComplaintsByIdAndType);
     const [ComplaintToBeEdited, setComplaintToBeEdited] = useState({});
-    
+
 
     const getUserComplaintsById = async () => {
         try {
@@ -46,7 +48,11 @@ export const ComplaintContextProvider = ({ children }) => {
             if (response.ok) {
                 setFetchComplaintsByIdAndType(responseData.data);
                 setcomplaintsToBeDisplayed(responseData.data);
+                setcomplaintnotPresent(false)
             } else {
+                setcomplaintnotPresent(true);
+                setcomplaintsToBeDisplayed({complaits:[], count:0})
+                setFetchComplaintsByIdAndType({complaits:[], count:0})
                 toast.error(responseData.message)
             };
 
@@ -59,22 +65,77 @@ export const ComplaintContextProvider = ({ children }) => {
         setFetchComplaintsById((prev) => [newComplaint, ...prev]);
     };
 
-    const filterFetchedComplaints = (state)=>{
-        const complaints = FetchedComplaintsByIdAndType?.complaints.filter((complaint)=> complaint.state === state);
-        const count = complaints.length 
+    const filterFetchedComplaints = (state) => {
+        const complaints = FetchedComplaintsByIdAndType?.complaints.filter((complaint) => complaint.state === state);
+        const count = complaints.length
         setcomplaintsToBeDisplayed({
             complaints,
             count
         });
     };
 
-    const getComplaintToBeEdited = (complaintId)=>{
-        
-        const FoundComplaint = complaintsToBeDisplayed?.complaints?.find((complaint)=>complaint._id === complaintId );
-        if(FoundComplaint){
+    const getComplaintToBeEdited = (complaintId) => {
+
+        const FoundComplaint = complaintsToBeDisplayed?.complaints?.find((complaint) => complaint._id === complaintId);
+        if (FoundComplaint) {
             setComplaintToBeEdited(FoundComplaint);
         }
     };
+
+    const deleteComplaint = async (complaintId) => {
+        console.log(complaintId)
+        try {
+            const response = await fetch(`http://localhost:8000/api/v1/complaints/${complaintId}/delete-complaint`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: Token
+                }
+            });
+
+            const responseData = await response.json();
+            console.log(responseData)
+            if (response.ok) {
+                const updatedComplaints = complaintsToBeDisplayed.complaints.filter(complaint => complaint._id !== complaintId);
+                setcomplaintsToBeDisplayed({
+                    complaints:updatedComplaints,
+                    count:updatedComplaints.length
+                })
+                toast.success(responseData.message);
+            } else {
+                toast.error(responseData.message)
+            }
+
+
+        } catch (error) {
+            console.log("complaintContext::deletecomplaint->", error.message)
+
+        }
+    };
+
+    
+
+    // useEffect(()=>{
+    //     if(complaintsMapstate){
+    //         console.log("i am complaintstate",complaintsMapstate)
+    //         const complaintsArray = Array.from(complaintsMap.values());
+    //         console.log(complaintsArray)
+    //         setcomplaintsToBeDisplayed({
+    //                 complaints:complaintsArray,
+    //                 count: complaintsArray.length
+    //             });
+    //     }
+    // },[complaintsMapstate])
+
+    // useEffect(() => {
+        
+    //     if (complaintsToBeDisplayed?.complaints?.length > 0) {
+    //         const complaintArray = complaintsToBeDisplayed.complaints
+    //         console.log(complaintArray)
+    //         const complaintsMap = new Map(complaintArray.map((complaint) => [complaint._id, complaint]));
+    //         setComplaintsMap(complaintsMap);
+           
+    //     }
+    // }, [complaintsToBeDisplayed]);
 
     useEffect(() => {
         AccessToken && getUserComplaintsById();
@@ -93,7 +154,9 @@ export const ComplaintContextProvider = ({ children }) => {
                     filterFetchedComplaints,
                     getComplaintToBeEdited,
                     ComplaintToBeEdited,
-                    Token
+                    Token,
+                    deleteComplaint,
+                    complaintsnotPresent
                 }
             }>
             {children}
@@ -104,3 +167,6 @@ export const ComplaintContextProvider = ({ children }) => {
 export const useComplaintContext = () => {
     return useContext(complaintContext);
 };
+
+
+   
