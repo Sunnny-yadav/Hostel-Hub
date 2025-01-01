fcb3d23206f8c0b5f5e39f091f3431be69af3b9d
sunny-yadav <sunny9405683@gmail.com>
2025-01-01 23:58:22 +0530

    added routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,11 +12,16 @@ import Login from './components/User_Forms/Login'
 import StudentDashboard from './components/Dashboards/Student/StudentDashboard'
 import MyComplaints from './components/Dashboards/Student/Complaint/MyComplaints'
 import Meal_Poll from './components/Dashboards/Student/Meal_Poll'
-import WarDash from './components/Dashboards/Warden/WarDash'
+import WardenDashboard from './components/Dashboards/Warden/WardenDashboard'
 import Dashboard from './components/Dashboards/Student/Dashboard'
 import MatchPartner from './components/Dashboards/Student/MatchPartner'
 import EditComplaint from './components/Dashboards/Student/Complaint/EditComplaint'
+import WarComments from './components/Dashboards/Warden/WarComments'
 import Comments from './components/Dashboards/Student/Complaint/Comments'
+import WardenStart from './components/Dashboards/Warden/WardenStart'
+import ViewComplaint from './components/Dashboards/Warden/ViewComplaint'
+import ComplaintDetails from './components/Dashboards/Warden/ComplaintDetails'
+import MealForm from './components/Dashboards/Warden/MealForm'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,19 +29,32 @@ const router = createBrowserRouter(
       <Route path="/" element={<Index />} />
       <Route path="/features" element={<Features />} />
       <Route path="/features/TermsPage" element={<TermsPage />} />
-      <Route path='/warden-dashboard' element={<WarDash />} />
+
+      {/* Login routes  */}
       <Route path='/Login_SignIn' element={<Login_SignIn_Layout />}>
         <Route path='/Login_SignIn/' element={<Registration />} />
         <Route path='/Login_SignIn/login' element={<Login />} />
       </Route>
+
+      {/* routes for student pages  */}
       <Route path="/student-dashboard" element={<StudentDashboard />}>
-        <Route path='/student-dashboard/' element={<Dashboard/>}/>
-        <Route path='/student-dashboard/raise-complaint' element={<RaiseComplaint_Form/>}/>
-        <Route path='/student-dashboard/vote' element={<Meal_Poll/>}/>
-        <Route path='/student-dashboard/find-match' element={<MatchPartner/>}/>
-        <Route path="/student-dashboard/review-complaints" element={<MyComplaints/>}/>
-        <Route path="/student-dashboard/:complaintId/edit-complaint" element={<EditComplaint/>}/>
-        <Route path="/student-dashboard/:complaintId/comments" element={<Comments/>}/>
+        <Route path='/student-dashboard/' element={<Dashboard />} />
+        <Route path='/student-dashboard/raise-complaint' element={<RaiseComplaint_Form />} />
+        <Route path='/student-dashboard/vote' element={<Meal_Poll />} />
+        <Route path='/student-dashboard/find-match' element={<MatchPartner />} />
+        <Route path="/student-dashboard/review-complaints" element={<MyComplaints />} />
+        <Route path="/student-dashboard/:complaintId/edit-complaint" element={<EditComplaint />} />
+        <Route path="/student-dashboard/:complaintId/comments" element={<Comments />} />
+      </Route>
+
+      {/* routes for the warden page  */}
+      <Route path='/warden-dashboard' element={<WardenDashboard />} >
+        <Route path='' element={<WardenStart />} />
+        <Route path='/warden-dashboard/view-complaints' element={<ViewComplaint />} />
+        <Route path='/warden-dashboard/add-meal' element={<MealForm/>}/>
+        <Route path='/warden-dashboard/complaintDetails' element={<ComplaintDetails />}>
+          <Route path='/warden-dashboard/complaintDetails/comments' element={<WarComments />} />
+        </Route>
       </Route>
 
     </>
